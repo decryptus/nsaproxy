46ce12eee58922f244a287d4ec1b2e046d931ebf
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2018-09-23 11:57:20 +0200

    [RELEASE] version: 0.1.23

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import find_packages, setup
 
 requirements = [line.strip() for line in open('requirements.txt', 'r').readlines()]
-version      = '0.1.21'
+version      = '0.1.23'
 
 if os.path.isfile('VERSION'):
     version = open('VERSION', 'r').readline().strip() or version
