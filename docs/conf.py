e20aee30c8d4764598bd12c30210438a640dacff
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2021-12-24 17:39:44 +0100

    Release version: 0.1.41

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'covenantdoc'
+htmlhelp_basename = 'nsaproxydoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -136,7 +136,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'covenant.tex', u'covenant Documentation',
+    (master_doc, 'nsaproxy.tex', u'nsaproxy Documentation',
      u'Adrien Delle Cave', 'manual'),
 ]
 
@@ -146,7 +146,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'covenant', u'covenant Documentation',
+    (master_doc, 'nsaproxy', u'nsaproxy Documentation',
      [author], 1)
 ]
 
@@ -157,8 +157,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'covenant', u'covenant Documentation',
-     author, 'covenant', 'One line description of project.',
+    (master_doc, 'nsaproxy', u'nsaproxy Documentation',
+     author, 'nsaproxy', 'One line description of project.',
      'Miscellaneous'),
 ]
 
